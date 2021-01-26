a58fb309f49f05519777205318362972ab43e7f8
joel <joel@afreecatv.com>
2021-01-26 11:32:01 +0900

    NG : Activity 에서 상태를 관리 NG : 전역 변수로 상태를 관리 NG : Activity가 상태를 분기해서 처리 NG : 상수값으로 상태를 관리

--- app/src/main/java/com/example/statemanager/MainActivity.kt
+++ app/src/main/java/com/example/statemanager/MainActivity.kt
@@ -2,10 +2,56 @@ package com.example.statemanager
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
+import com.example.statemanager.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    //NG : Activity 에서 상태를 관리
+    //NG : 전역 변수로 상태를 관리
+    var state = STATE_BLACK
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        init()
+    }
+
+    private fun init() {
+        binding.button1.setOnClickListener {
+            state = STATE_BLACK
+            updateInfo(state)
+
+        }
+        binding.button2.setOnClickListener {
+            state = STATE_RED
+            updateInfo(state)
+        }
+        binding.button3.setOnClickListener {
+            state = STATE_BLUE
+            updateInfo(state)
+        }
+    }
+
+    //NG : Activity가 상태를 분기해서 처리
+    private fun updateInfo(state: Int) {
+        when(state) {
+            STATE_BLACK -> binding.image.setBackgroundColor(ContextCompat.getColor(baseContext ,R.color.black))
+            STATE_RED -> binding.image.setBackgroundColor(ContextCompat.getColor(baseContext ,R.color.red))
+            STATE_BLUE -> binding.image.setBackgroundColor(ContextCompat.getColor(baseContext ,R.color.blue))
+            else -> throw IllegalStateException()
+        }
+        binding.image
+    }
+
+    companion object {
+        //NG : 상수값으로 상태를 관리
+        const val STATE_BLACK = 1
+        const val STATE_RED = 2
+        const val STATE_BLUE = 3
     }
 }
